68d7992b048236cdb290e1fad18652f8b5d93480
sheldon fletcher <shelcodingdon@gmail.com>
2021-06-25 15:44:37 -0400

    added local storage to match the hour block

--- script.js
+++ script.js
@@ -40,3 +40,13 @@ $(".saveBtn").click(function(){
 
 // reshesrch how add and remove a class in jquery
 colorizer()
+
+ $("#block-9 .description").val(localStorage.getItem("9am"))
+ $("#block-10 .description").val(localStorage.getItem("10am"))
+ $("#block-11 .description").val(localStorage.getItem("11am"))
+ $("#block-12 .description").val(localStorage.getItem("12am"))
+ $("#block-13 .description").val(localStorage.getItem("1pm"))
+ $("#block-14 .description").val(localStorage.getItem("2pm"))
+ $("#block-15 .description").val(localStorage.getItem("3pm"))
+ $("#block-16 .description").val(localStorage.getItem("4pm"))
+ $("#block-17 .description").val(localStorage.getItem("5pm"))
